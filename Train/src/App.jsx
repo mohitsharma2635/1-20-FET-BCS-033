72b67270508739392789475d505a65c6801ff006
Mohit Sharma <mohit.ms2603@gmail.com>
2023-07-19 20:28:39 +0530

    final commit

--- Train/src/App.jsx
+++ Train/src/App.jsx
@@ -1,50 +1,68 @@
-import React, { useEffect } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import TrainDetails from "./Pages/TrainDetails.jsx";
-import AllTrains from "./Pages/AllTrains.jsx";
+import { useEffect } from "react";
+
 import axios from "axios";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AllTrains />,
-  },
-  {
-    path: "/:trainNumber",
-    element: <TrainDetails />,
-  },
-]);
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import AllTrains from "./Pages/AllTrains";
+import TrainDetails from "./Pages/TrainDetails";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<AllTrains />} />
+      <Route path=":trainNumber" element={<TrainDetails />} />
+    </Route>
+  )
+);
+
+const timestampToMinutes = (timestamp) => {
+  const milliseconds = timestamp * 1000;
+  const now = Date.now();
+  const difference = now - milliseconds;
+  const minutes = difference / (1000 * 60);
+  return minutes;
+};
+
+const getToken = async () => {
+  const body = {
+    companyName: "Mohit Central",
+    clientID: "ef244808-e51b-4a47-b72d-9db150e13daa",
+    clientSecret: "iIzWcEZsdyUJTDAO",
+    ownerName: "Mohit Sharma",
+    ownerEmail: "ram@abc.edu",
+    rollNo: "33",
+  };
+
+  const { data } = await axios.post("http://20.244.56.144/train/auth", body);
+
+  const expirationTime = Date.now() + data.expires_in * 1000;
+
+  localStorage.setItem("token", data.access_token);
+  localStorage.setItem("expiry", expirationTime);
+};
+
+const checkAndFetchTokenIfNeeded = () => {
+  const token = localStorage.getItem("token");
+  const expiry = localStorage.getItem("expiry");
+  const minutesSinceExpiry = expiry ? timestampToMinutes(expiry) : null;
+
+  if (!token || minutesSinceExpiry === null || minutesSinceExpiry <= 5) {
+    getToken();
+  }
+};
 
 function App() {
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    const getToken = async () => {
-      const body = {
-        companyName: "Mohit Central",
-        clientID: "ef244808-e51b-4a47-b72d-9db150e13daa",
-        clientSecret: "iIzWcEZsdyUJTDAO",
-        ownerName: "Mohit Sharma",
-        ownerEmail: "ram@abc.edu",
-        rollNo: "33",
-      };
-
-      const { data } = await axios.post(
-        "http://20.244.56.144/train/auth",
-        body
-      );
-      localStorage.setItem("token", data.access_token);
-    };
-
-    if (!token) {
-      getToken();
-    }
+    checkAndFetchTokenIfNeeded();
   }, []);
 
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
